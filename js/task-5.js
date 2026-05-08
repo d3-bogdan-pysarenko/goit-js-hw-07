3bb97b4fdfdd615f27586943c25a85757623e412
Bogdan Pysarenko <bogdan.pysarenko@door3.com>
2026-05-08 15:13:39 +0300

    5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bgBody = document.body;
+const btnChangeColor = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', () => {
+  const randomHexColor = getRandomHexColor();
+  bgBody.style.backgroundColor = randomHexColor;
+  color.textContent = randomHexColor;
+});
